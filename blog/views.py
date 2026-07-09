d8c08426de3bea0212d10ad70551cb42a97eec4c
Inés <inesperezesquiroz@gmail.com>
2026-07-09 17:10:33 +0200

    Add new template

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    post=Post.objects.filter(published_date__lte=timezone.now()).orderby("published_date")
+    post=Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
     return render(request, 'blog/post_list.html', {"post": post})
